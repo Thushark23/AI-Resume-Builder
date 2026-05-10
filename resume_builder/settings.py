c126e98c3ba26406192f20f70d06b2f0a9f200f5
Thushar <thushark235@gmail.com>
2026-05-10 21:53:57 +0530

    Fix CSRF trusted origins for Railway

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -65,3 +65,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'resume_app', 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+CSRF_TRUSTED_ORIGINS = ['https://ai-resume-builder-production-cb7e.up.railway.app']
